a43ffbf3ebacaf179c041dfd0c93cfbb3601a104
oladeha2 <ameenbello@gmail.com>
2019-07-24 16:05:08 +0100

    get baseline pre-trained resnet

--- baseline.py
+++ baseline.py
@@ -18,7 +18,7 @@ trans = transforms.Compose([
     transforms.RandomResizedCrop(224),
     transforms.RandomHorizontalFlip(),
     # convert to tensor for input into the model
-    transforms.toTensor(),
+    transforms.ToTensor(),
     # normalise the images last of all
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
