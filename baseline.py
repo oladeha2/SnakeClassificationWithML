1d0422b59e409d01caf76bfa2fc0bc43c371b490
oladeha2 <ameenbello@gmail.com>
2019-07-25 10:33:58 +0100

    get baseline results using resnet 101

--- baseline.py
+++ baseline.py
@@ -1,9 +1,17 @@
 # use RESNET101 as a baseline for the snake classification problem
 
-import torchvision.models.resnet as RESNET101
+import torchvision.models as models
 import torch.utils.data as torchdata
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
+import torch.nn as nn
+import torch
+
+import numpy as np
+
+# set random seeds
+torch.manual_seed(271828)
+np.random.seed(271828)
 
 
 # load data and generate transforms for the data
@@ -29,7 +37,20 @@ train_data = datasets.ImageFolder(train_dir, trans)
 valid_data = datasets.ImageFolder(valid_dir, trans)
 
 print('Length of Training set -> ', len(train_data))
-print('Length of Validation set', len(valid_data))
+print('Length of Validation set -> ', len(valid_data))
 
 print('Classes -> ',  train_data.classes)
+number_of_classes = len(train_data.classes)
 
+#load the data using data loader
+train_loader = torchdata.DataLoader(train_data, batch_size=32, shuffle=True, num_workers=2)
+valid_loader = torchdata.DataLoader(valid_data, batch_size=32, shuffle=True, num_workers=2)
+
+# define the loss function
+loss_function = nn.CrossEntropyLoss()
+
+# using resnet101 pretrained to get the baseline 
+resnet = models.resnet101(pretrained=True, progress=True)
+
+# construct an optimizer, this must be based on the number of parameters you are attempting to train in the pre-trained model
+# try retraining just the final layer for the task, leaving the pretrained network as a feature extractor
---
+++ train.py
@@ -0,0 +1,1 @@
+# python file containing the training function for a model
