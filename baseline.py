c99962c26a981794184e1e63fd7958e46559ae42
oladeha2 <ameenbello@gmail.com>
2019-07-25 11:33:02 +0100

    get baseline results using resnet 101

--- baseline.py
+++ baseline.py
@@ -5,14 +5,17 @@ import torch.utils.data as torchdata
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 import torch.nn as nn
+import torch.optim as optim
 import torch
 
 import numpy as np
+import os
 
 # set random seeds
 torch.manual_seed(271828)
 np.random.seed(271828)
 
+device =  'cuda'
 
 # load data and generate transforms for the data
 
@@ -46,11 +49,27 @@ number_of_classes = len(train_data.classes)
 train_loader = torchdata.DataLoader(train_data, batch_size=32, shuffle=True, num_workers=2)
 valid_loader = torchdata.DataLoader(valid_data, batch_size=32, shuffle=True, num_workers=2)
 
+
 # define the loss function
 loss_function = nn.CrossEntropyLoss()
 
 # using resnet101 pretrained to get the baseline 
+# load the model and edit for the number of classes required for snake classification (45 classes)
 resnet = models.resnet101(pretrained=True, progress=True)
+print('Resnet Model Loaded')
 
-# construct an optimizer, this must be based on the number of parameters you are attempting to train in the pre-trained model
-# try retraining just the final layer for the task, leaving the pretrained network as a feature extractor
+# in_features is the number of features for the Linear Layer
+num_features = resnet.fc.in_features
+# edit the final layer matrix for the new number of classes
+resnet.fc = nn.Linear(num_features, number_of_classes)
+resnet.to(device)
+
+# construct the optimizer --> optimizer for the gradient descent
+optimizer = optim.Adam(resnet.parameters(), lr=0.001, momentum=0.7) # --> test initial loss rate as 0.01 change subject to results and speed of learning during training
+
+# define a loss rate scheduler that decays that reduces the learning rate as the number of epochs increases, prevents the learning rate fron bouncing up and down as the network trains
+lr_sched = optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+
+# train the model end to end as opoosed to just training the final classification layer of the model
+
+# train the model using the folllowing parameters --> model, loss_function, loss rate scheduler, train data and validation data and the number of epochs -> not saving the checkpoints
--- train.py
+++ train.py
@@ -1,1 +1,24 @@
-# python file containing the training function for a model
+import torch
+import time
+import copy
+
+# train model function
+def train_model(model, loss_function, scheduler, num_epochs, train_data, valid_data):
+
+    # create data dictionary --> using data loaders
+
+    data_dict : { 
+        'train': train_data,
+        'valid': valid_data        
+    }
+
+    start = time.time()
+
+    best_model_weights = copy.deepcopy(model.state_dict)
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs-1))
+        print('-' * 10)
+
+
